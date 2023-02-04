55178875bff8c1c3df56586b15361170c643cef0
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-02-03 19:25:53 -0600

    Add a few variables assigned to numbers

--- variables.js
+++ variables.js
@@ -3,3 +3,7 @@
 var songName = "TentpoleShangrila"
 var songName = "JustAlongForTheRide"
 var songName = "Chateau(FeelAlright)"
+
+var songLength = 3.36
+var songLength = 3.21
+var songLength = 4.32

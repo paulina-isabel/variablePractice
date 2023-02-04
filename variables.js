50c62497a76054740ad3b0aebfe6140eea506938
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-02-03 19:27:41 -0600

    Add a few vasiables assignmed to Booleans

--- variables.js
+++ variables.js
@@ -7,3 +7,7 @@ var songName = "Chateau(FeelAlright)"
 var songLength = 3.36
 var songLength = 3.21
 var songLength = 4.32
+
+var addedToPlaylist = true
+var addedToPlaylist = true
+var addedToPlaylist = false
